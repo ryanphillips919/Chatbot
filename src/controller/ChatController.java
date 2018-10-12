96d95c7b6933199656c808f15ec3c205d28e6e06
rphi8796 <jlkfop@gmail.com>
2018-10-12 13:53:36 -0600

    I finished the model and added the classes to the controller

--- src/controller/ChatController.java
+++ src/controller/ChatController.java
@@ -1,11 +1,45 @@
 package controller;
 
+import javax.swing.JOptionPane;
+
+import model.Chatbot;
+
 public class ChatController
 {
+//	private Chatbot simpleBot;
+	
+	
+//_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\\
+	
+	
+	public ChatController()
+	{
+//		simpleBot = new Chatbot();
+	}
+	
+	
+//_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\\
 	
 	
 	public void start()
 	{
-		
+		String userInput = JOptionPane.showInputDialog(null, "Hello, how are you?");
+		JOptionPane.showMessageDialog(null, userInput);
+		while (!userInput.equalsIgnoreCase("quit"));
+		{
+			userInput = JOptionPane.showInputDialog(null, "Type quit if you are done");
+		}
+	}
+	
+	
+//_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\\
+	
+	
+	public String interactWithChatbot(String userInput)
+	{
+		return null;
 	}
 }
+
+
+//_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\\
--- src/model/Chatbot.java
+++ src/model/Chatbot.java
@@ -4,8 +4,87 @@ import java.util.ArrayList;
 
 public class Chatbot
 {
-ArrayList<String> responseList;
-String content;
+	private ArrayList<String> responseList;
+	private String content;
+	private String currentUser;
+	private String joke;
 
 	
+//_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\\
+	
+	
+	public Chatbot()
+	{
+		
+	}
+	
+	
+//_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\\
+	
+	
+	public Chatbot(String content) 
+	{
+		this.content = content;
+	}
+	
+	
+//_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\\
+	
+	
+	public ArrayList<String> getResponseList()
+	{
+		return responseList;
+	}
+	
+	public String getContent()
+	{
+		return content;
+	}
+	
+	public String getCurrentUser()
+	{
+		return currentUser;
+	}
+	
+	public String getJoke()
+	{
+		return joke;
+	}
+	
+	
+//_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\\	
+	
+	
+	public void setResponseList(ArrayList<String> responseList)
+	{
+		this.responseList = responseList;
+	}
+	
+	public void setContent(String content)
+	{
+		this.content = content;
+	}
+	
+	public void setCurrentUser(String currentUser)
+	{
+		this.currentUser = currentUser;
+	}
+	
+	public void setJoke(String joke)
+	{
+		this.joke = joke;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
