176450c74d449b27df57d7f41f484650715d71bd
rphi8796 <jlkfop@gmail.com>
2019-04-23 09:50:53 -0600

    I added some imports for twitter and instantiated the factory method

--- src/chat/model/ChatTwitter.java
+++ src/chat/model/ChatTwitter.java
@@ -1,13 +1,17 @@
 package chat.model;
 
 import chat.controller.ChatController;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
 
 public class ChatTwitter
 {
 	private ChatController app;
+	private Twitter chatTwitter;
 	
 	public ChatTwitter(ChatController app)
 	{
 		this.app = app;
+		this.chatTwitter = TwitterFactory.getSingleton();
 	}
 }
